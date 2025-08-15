6a4c600f1f016872337a52a8f6dd8bda868d4ef1
sh <saab35@naver.com>
2025-08-16 00:47:28 +0900

    feat: BaseTime (생성날짜, 수정날짜) 추가

---
+++ src/main/java/com/sport/backend/global/Entity/BaseTime.java
@@ -0,0 +1,24 @@
+package com.sport.backend.global.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
